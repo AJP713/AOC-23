c7baaf5362bc292d5017c0745ac593f208f6950b
AJP713 <64868729+AJP713@users.noreply.github.com>
2023-12-13 14:36:23 -0500

    Update AOC-23-2a.js

--- AOC-23-2a.js
+++ AOC-23-2a.js
@@ -2,7 +2,7 @@ let data = "Game 1: 12 blue; 2 green, 13 blue, 19 red; 13 red, 3 green, 14 blue@
 
 let dataShort = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green@Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue@Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red@Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red@Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 data = data.replaceAll("Game ","#")
-data = data.replaceAll("green","geen")
+data = data.replaceAll("green","geen")  // so I got rid of the "r" in green so I can just refer to red as r.  
 data = data.replaceAll(":",";")
 data = data.split("@")
 console.log(data)
